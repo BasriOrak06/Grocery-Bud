1b733c80137edd8ab11bb1680b6fd0dde307c69e
unknown <basriorak@aof.anadolu.edu.tr>
2023-11-03 18:40:42 +0300

    first commit

---
+++ src/Form.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+
+const Form = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!newItemName) {
+      toast.error('Please enter an item');
+      return;
+    }
+    addItem(newItemName);
+    setNewItemName('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h4>Grocery Bud</h4>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='form-input'
+            value={newItemName}
+            onChange={(event) => setNewItemName(event.target.value)}
+          />
+          <button type='submit' className='btn'>
+            add item
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
